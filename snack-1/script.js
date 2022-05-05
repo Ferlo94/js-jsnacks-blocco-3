522c2a81c576dfa4d0386cf75a37ecc60314fefd
Ferlo94 <laferlart@gmail.com>
2022-05-05 16:00:39 +0200

    check numero

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,17 +1,24 @@
 const array = [];
 
-
+const userNumber = Number(prompt("Inserisci un numero"));
 
 let somma = 0;
 
 while(somma < 50) {
 
+    if (isNaN(userNumber)) {
+        userNumber = Number(prompt("Inserisci un numero!!!!!!"));
+    } else {
+
     const userNumber = Number(prompt("Inserisci un numero"));
     somma += userNumber;
     array.push(userNumber);
+
+    console.log(array);
+    }
 }
 
-console.log(somma, array)
+console.log(somma)
 
 
 
---
+++ snack-2/script.js
@@ -0,0 +1,15 @@
+// Fai inserire un numero, che chiameremo N, all’utente.
+// Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100. Ogni volta che ne crei uno, stampalo a schermo.
+
+
+
+let N = Number(prompt("Inserisci un numero"));
+
+const arr1 = [];
+
+
+
+while (isNaN(N)) {
+  N = Number(prompt("Inserisci un numerooooooooooooo!!!"));
+}
+
