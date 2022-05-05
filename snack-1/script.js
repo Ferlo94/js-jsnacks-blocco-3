4cdb449d98c29c608645354b07027910e716ca44
Ferlo94 <laferlart@gmail.com>
2022-05-05 12:25:42 +0200

    first commit

---
+++ snack-1/script.js
@@ -0,0 +1,23 @@
+const array = [];
+
+
+
+let somma = 0;
+
+while(somma < 50) {
+
+    const userNumber = Number(prompt("Inserisci un numero"));
+    somma += userNumber;
+    array.push(userNumber);
+}
+
+console.log(somma, array)
+
+
+
+
+
+
+
+
+
